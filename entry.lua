acb35edee54d165b8dd037dbd1baf1908f7cc340
jjjjj103074 <96659427+jjjjj103074@users.noreply.github.com>
2024-03-22 18:43:58 +0800

    能開啟的狀態

--- entry.lua
+++ entry.lua
@@ -1,31 +1,56 @@
--- entry.lua
-require("test_function")
--- 定義F-CK-1-C飛機模組
-FCK1C = {
-    displayName     = _("F-CK-1-C"),
-    developerName   = "jjjjj103074",
-    version         = "0.1.0",
-    info            = _("我懶得想"),
-    state           = "installed",
-    dirName         = current_mod_path,
-    InputProfiles   = {
-        ["f-ck-1-c"] = current_mod_path .. '/Input/f-ck-1-c',
-    },
-    Skins           = {
-        { name = _("Default"), dir = "Skins/Default" },
-        { name = _("Custom"), dir = "Skins/Custom" }
-    },
-    Missions        = {
-        { name = _("Training"), dir = "Missions/Training" },
-        { name = _("Combat"), dir = "Missions/Combat" }
-    },
-    LogBook         = {
-        { name = _("F-CK-1-C"), type = "FCK1C" }
-    },
-    Options         = {
-        { name = _("F-CK-1-C Options"), dir = "Options", CLSID = "{FCK1C options}" }
-    }
-}
+local self_ID="F-CK-1-C"
+-- M.write_log("開始載入模組")
+declare_plugin(self_ID,
+{
+installed 	 = true,
+dirName	  	 = current_mod_path,
+displayName  = _("F-CK-1-C"),
 
--- 定義模組完成
-plugin_done()
+fileMenuName = _("F-CK-1-C"),
+update_id        = "F-CK-1-C",
+version		 = "0.1.0",
+state		 = "installed",
+info		 = _("F-CK-1-C test mod"),
+encyclopedia_path = current_mod_path..'/Encyclopedia',
+-- binaries	 = bin,
+
+Skins	=
+	{
+		{
+			name	= _("F-CK-1-C"),
+			dir		= "Skins/1"
+		},
+	},
+Missions =
+	{
+		{
+			name		= _("F-CK-1-C"),
+			dir			= "Missions",
+			--CLSID		= "{CLSID5456456346CLSID}",
+		},
+	},
+LogBook =
+	{
+		{
+			name		= _("F-CK-1-C"),
+			type		= "F-CK-1-C",
+		},
+	},
+InputProfiles =
+	{
+		["F-CK-1-C"]     = current_mod_path .. '/Input',
+	},
+Options =
+    {
+        {
+            name		= _("F-CK-1-C"),
+            nameId		= "F-CK-1-C",
+            dir			= "Options",
+            CLSID		= "{F-CK-1-C options}"
+        },
+    },
+
+})
+---------------------------------------------------------------------------------------
+
+plugin_done()-- finish declaration , clear temporal data
